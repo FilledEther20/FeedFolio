a188a97f8c48ef149a3bdf9ef8943267ca0c476c
FilledEther <gairolachaitanya@gmail.com>
2024-06-08 02:28:02 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
+const feedRoutes=require('./routes/routes')
 app.use(express.json());
 app.use(cors());
+
+app.use('/',feedRoutes)
+
+
 app.listen(PORT, () => {
 	console.log(`Listenning on port : ${PORT}`);
 });
